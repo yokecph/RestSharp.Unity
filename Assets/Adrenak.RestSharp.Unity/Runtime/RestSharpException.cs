7455bc532ab89e2e22a263c5544d0d48151d4a74
Sune Kastbjerg <sune@yoke.dk>
2023-08-02 13:43:30 +0200

    Added missing dependencies

--- Assets/Adrenak.RestSharp.Unity/Runtime/RestSharpException.cs
+++ Assets/Adrenak.RestSharp.Unity/Runtime/RestSharpException.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Net;
 
-namespace RestSharp {
+namespace RestSharp
+{
     /// <summary>
-    /// Represents a RestSharp exception with status code, URI and 
+    ///     Represents a RestSharp exception with status code, URI and
     /// </summary>
-    public class RestSharpException : Exception {
-        public HttpStatusCode HttpStatusCode { get; private set; }
-        public Uri Uri { get; private set; }
-        public string Error { get; private set; }
-
-        public RestSharpException(HttpStatusCode httpStatusCode, Uri requestUri, string content, string error, Exception innerException)
-            : base(error, innerException) {
+    public class RestSharpException : Exception
+    {
+        public RestSharpException(HttpStatusCode httpStatusCode, Uri requestUri, string content, string error,
+                                  Exception      innerException)
+            : base(error, innerException)
+        {
             HttpStatusCode = httpStatusCode;
             Uri = requestUri;
             Error = content;
         }
+
+
+        public string         Error          { get; private set; }
+        public HttpStatusCode HttpStatusCode { get; private set; }
+        public Uri            Uri            { get; private set; }
     }
 }
